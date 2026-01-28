f6a35b8b16807a5ee0e2d2933e4a8f1b483c0b98
foyou <yimi.0822@qq.com>
2026-01-28 20:53:49 +0800

    feat: update author and repository info in executable metadata

--- build.rs
+++ build.rs
@@ -2,8 +2,7 @@ use std::io;
 
 fn main() -> io::Result<()> {
     if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        // winres automatically uses package name, version, description from Cargo.toml
+        let res = winres::WindowsResource::new();
         res.compile()?;
     }
     Ok(())
