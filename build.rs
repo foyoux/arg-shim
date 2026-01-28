df007a5d7e0f06049ff07766af92ca23b330be5e
foyou <yimi.0822@qq.com>
2026-01-28 20:55:01 +0800

    feat: explicitly embed author and repo URL into exe metadata

--- build.rs
+++ build.rs
@@ -2,7 +2,12 @@ use std::io;
 
 fn main() -> io::Result<()> {
     if cfg!(target_os = "windows") {
-        let res = winres::WindowsResource::new();
+        let mut res = winres::WindowsResource::new();
+        // 显式设置 Windows 资源信息
+        res.set("CompanyName", "foyou");
+        res.set("FileDescription", "arg-shim - A flexible CLI argument transformer");
+        res.set("LegalCopyright", "Copyright (c) 2025 foyou");
+        res.set("Comments", "https://github.com/foyoux/arg-shim");
         res.compile()?;
     }
     Ok(())
