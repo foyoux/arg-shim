917cc7fb7bda7e7c23f7f3719c61a414fd6f559c
foyou <yimi.0822@qq.com>
2026-01-28 20:56:12 +0800

    docs: update copyright years to 2025-2026

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() -> io::Result<()> {
         // 显式设置 Windows 资源信息
         res.set("CompanyName", "foyou");
         res.set("FileDescription", "arg-shim - A flexible CLI argument transformer");
-        res.set("LegalCopyright", "Copyright (c) 2025 foyou");
+        res.set("LegalCopyright", "Copyright (c) 2025-2026 foyou");
         res.set("Comments", "https://github.com/foyoux/arg-shim");
         res.compile()?;
     }
